bc58ca09182540c46c80d1b7bdb6defcf9426e0c
dbins <bins@ig.com.br>
2017-10-09 20:51:41 -0300

    2017-10-09

--- controllers/promocoes.js
+++ controllers/promocoes.js
@@ -33,21 +33,22 @@ module.exports = function (app){
 	app.get("/promocoes/promocao/:nomedapromocao", function(req,res){
 		var nomedapromocao = req.params.nomedapromocao;
 		var api = new webservice(app.locals.shopping);
-		var consulta = api.view(nomedapromocao).then(function (resultados) {
-			var consulta2 = api.list().then(function (resultados2) {
+		//var consulta = api.view(nomedapromocao).then(function (resultados) {
+		//	var consulta2 = api.list().then(function (resultados2) {
+		//		if (typeof resultados.dados.info.title === undefined) {
+					//res.status(500).redirect('/erro/500');
+		//		} else {
+					//res.render("promocoes/promocao", {resultados:resultados.dados, outros:resultados2.dados.data,moment: moment});
+					res.render("promocoes/promocao", {resultados:{}, outros:{}	,moment: moment});
+					
+		//		}
 				
-				if (typeof resultados.dados.info.title === undefined) {
-					res.status(500).redirect('/erro/500');
-				} else {
-					res.render("promocoes/promocao", {resultados:resultados.dados, outros:resultados2.dados.data,moment: moment});
-				}
-				
-			}).catch(function (erro){
-				res.status(500).redirect('/erro/500');
-			});
-		}).catch(function (erro){
-			res.status(500).redirect('/erro/500');
-		});
+		//	}).catch(function (erro){
+		//		res.status(500).redirect('/erro/500');
+		//	});
+		//}).catch(function (erro){
+		//	res.status(500).redirect('/erro/500');
+		//});
 	});
 	
 	app.get("/promocoes/favoritar/:nomedapromocao", function(req,res){
